d67c337f822d132abea79b95e41bb8a76d2d7a25
Justin Howard <justin@justinhoward.co.uk>
2016-12-13 14:24:21 +0000

    Updated port

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use(routes);
 app.use(contactRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server listening on port 3000");
-})
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
